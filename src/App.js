a224b2cb878e6af2aa36d13474b8d5fd67b587e1
FoolCoder <qazafi.sheikh121@gmail.com>
2025-02-15 11:21:43 +0000

    update text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          First React app deployed on AWS Amplify
+          First React app deployed on Vercel
         </a>
       </header>
     </div>
